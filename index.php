a8726bce8c9800f4b1aac7daaa2d57ef573cc262
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2020-12-01 08:53:00 +0100

    First working version.

--- index.php
+++ index.php
@@ -12,12 +12,14 @@ if(!file_exists($csvPath)){
     //$xml_src = file_get_contents("test.xml");
     $xml = simplexml_load_string($xml_src, null, LIBXML_NOCDATA);
 
+    /*
     echo $api_url;
     echo "<pre>";
     var_dump($xml);
     echo "</pre>";
+    */
 
-    $csv = generateCSV($xml, $lastDate); 
+    $csv = generateCSV($xml, $api_url_accept); 
 
     if($csv !== null){
 		writeCsv($csv, $csvPath);		
@@ -25,7 +27,6 @@ if(!file_exists($csvPath)){
     else{
 	    echo "No new orders!";
     }
-    writeDate($datePath);
 }
 else{
 	echo "CSV file was not processed yet!";
@@ -42,17 +43,58 @@ else{
  * @param string    $xml    The xml you want to parse to csv.
  * @return string   $csv    The parsed csv. 
 */
-function generateCSV($xml, $lastDate){
+function generateCSV($xml, $api_url_accept){
     
-    $csv = null;
+    $csv = "";
+
+    //TODO: Multiple products
+    //TODO: Time management
+    //TODO: Accept orders?
+
+
+    foreach($xml->datas[0]->order as $orderline){
+        /* echo "<pre>";
+        var_dump($orderline);
+        echo "</pre>"; */
+
+        foreach($orderline->products->product as $productline){
+            $csv = $csv . $orderline->order_ref . ";";
+            $csv = $csv . $orderline->order_time . ";";
+            $csv = $csv . $orderline->billing_address->email . ";";
+            $csv = $csv . $productline->sku . ";";
+            $csv = $csv . $productline->quantity . ";";
+            $csv = $csv . $productline->price . ";";
+            $csv = $csv . $orderline->shipping_address->firstname . " " . $orderline->shipping_address->lastname . ";";
+            $csv = $csv . $orderline->shipping_address->company . ";";
+            $csv = $csv . $orderline->shipping_address->address_1 . ";";
+            $csv = $csv . $orderline->shipping_address->zipcode . ";";
+            $csv = $csv . $orderline->shipping_address->city . ";";
+            $csv = $csv . $orderline->shipping_address->country_iso . ";";
+            $csv = $csv . $orderline->billing_address->firstname . " " . $orderline->billing_address->lastname . ";";
+            $csv = $csv . $orderline->billing_address->company . ";";
+            $csv = $csv . $orderline->billing_address->address_1 . ";";
+            $csv = $csv . $orderline->billing_address->zipcode . ";";
+            $csv = $csv . $orderline->billing_address->city . ";";
+            $csv = $csv . $orderline->billing_address->country_iso . ";";
+            $csv = $csv . $orderline->billing_address->phone . ";";
+            $csv = $csv . $orderline->payment_solution . ";";
+            $csv = $csv . $orderline->shipping_price;
+            $csv = $csv . "\r\n";
+        }
+
+        $api_url = str_replace("%ORDERREF%", $orderline->order_ref, $api_url_accept);
+        $xml_src = file_get_contents($api_url);      
+    }
+
+    echo $csv;
     
-    
-    if($csv !== null){
-	    $csv = generateCSVHeadline().$csv;
+    if($csv !== ""){
+        $csv = generateCSVHeadline().$csv;
+        return $csv;
     }
     
     
-    return $csv;
+    return null;
 }
 
 /*
@@ -68,19 +110,8 @@ function generateCSVHeadline(){
             . "DeliveryZIP;DeliveryCity;DeliveryCountry;"
             . "InvoiceClient;InvoiceClient2;InvoiceStreet;"
             . "InvoiceZIP;InvoiceCity;InvoiceCountry;"
-            . "Phone;PaymentType;Shipping";
+            . "Phone;PaymentType;Shipping" . "\r\n";
     return $csv_headline;
-}
-
-/*
- * Writes the current DateTime to a file.
-*/
-function writeDate($datePath){
-    $date = new DateTime();
-    
-    $fp = fopen($datePath, 'w');
-    fwrite($fp, $date->format('Y-m-d H:i:s'));
-    fclose($fp);
 }
 
 /*
--- settings.sample.php
+++ settings.sample.php
@@ -5,25 +5,17 @@
  */
 
 $api_base = "https://ws.monechelle.com/?login=%APIUSER%&password=%APIPASS%&method=get_orders";
+$api_base_accept = "https://ws.monechelle.com/?login=%APIUSER%&password=%APIPASS%&method=accept_order&order_ref=%ORDERREF%";
 $api_user = "";
 $api_pass = "";
 
 $api_url = str_replace("%APIUSER%", $api_user, $api_base);
 $api_url = str_replace("%APIPASS%", $api_pass, $api_url);
 
+$api_url_accept = str_replace("%APIUSER%", $api_user, $api_base_accept);
+$api_url_accept = str_replace("%APIPASS%", $api_pass, $api_url_accept);
+
 $csvPath = "..\manomanoOrder.csv";
 $datePath = "date.txt";
 
 date_default_timezone_set('Europe/Berlin');
-
-$lastDate = readDate($datePath);
-$lastDateStr = $lastDate->format('Y-m-d H:i:s');
-
-
-
-
-function readDate($datePath){
-    $datetime = file_get_contents($datePath); 
-    $date = new DateTime($datetime);
-    return $date;
-}
